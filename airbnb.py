9371eacc60f3996f7d965561a6b94f5b7d3897f0
Suristar <56325899+Suristar@users.noreply.github.com>
2019-10-08 20:11:01 -0400

    Delete airbnb.py

--- airbnb.py
+++
@@ -1,7 +0,0 @@
-f = open('c:/users/surajit/desktop/first_10.csv')
-for line in f:
-    a=line.split(",")
-    x = a[9]
-    if(x != 'price'):
-        if int(x) >= 100 and int(x) <=150:
-            print(line)
